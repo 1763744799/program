9bd61e3d069e4e47a1ab6c6577c322777bbe718f
1763744799 <1763744799@qq.com>
2018-10-18 09:17:10 +0800

    登录

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //1,引入express
 var express = require('express');
 var app = express();
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+var session = require('express-session');
+app.use(session({
+    secret: '12345',
+    name: 'express_11_cookie',   //这里的name值得是cookie的name，默认cookie的name是：connect.sid
+    cookie: {maxAge: 24*60*60*1000 },     //设置maxAge是80000ms，即80s后session和相应的cookie失效过期
+}));
 var path = require('path');
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({extended:false});
@@ -15,15 +23,21 @@ app.engine('html', require('ejs').__express);
 //6,静态文件
 app.use(express.static('public'));
 
-var indexController=require('./Controllers/IndexController');
-app.get('/index',indexController.index);
-app.post('/login',urlencodedParser,indexController.login);
-var indexController = require('./Controllers/IndexController1');
-app.post('/signup',urlencodedParser,indexController.signup);
+var loginController=require('./Controllers/LoginController');
+
+ app.get('/login',loginController.login);
+app.post('/login',urlencodedParser,loginController.loginCheck);
 
 
 
+var signupController = require('./Controllers/SignupController1');
+app.post('/signup',urlencodedParser,signupController.signup);
 
+
+
+app.get('/index',function (req,res) {
+    res.render("index");
+})
 app.get('/login',function (req,res) {
     res.render("login");
 });
@@ -49,14 +63,17 @@ app.get('/product-m',function (req,res) {
 app.get('/shortcodes',function (req,res) {
     res.render("shortcodes");
 });
-app.get('/signup',function (req,res) {
-    res.render("signup");
-});
+
 app.get('/single',function (req,res) {
     res.render("single");
 });
 app.get('/terms',function (req,res) {
     res.render("terms");
 });
+app.get('/signup',function (req,res) {
+    res.render("signup");
+});
 
-app.listen(9999);
+app.listen(9999,function () {
+    console.log("启动")
+});
